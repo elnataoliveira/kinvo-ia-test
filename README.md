506dec46b82b9511d322712d2e2cb25bc31e2390
horizon iot <horizon@localhost.localdomain>
2020-03-09 20:09:15 -0300

    adcionado templates

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-end-points
+
+iniciar o app:
+
+	$flask run
+
+end-points:
 
 	- minerar e salvar as noticías;
+
 		http://localhost:<port>/crawl
+
 	- extrair as entidades das notícias mineradas(entity recognition);
+	
 		http://localhost:<port>/extract
 
--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import crochet
 #iniciar crochet
 crochet.setup()     
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 from scrapy.crawler import CrawlerRunner
 from feedsnews import NewsSpider
 import re
 import spacy  
 
 app = Flask('Scrape With Flask')
-#rodar crawl
+#atribui CrwalerRunner a variável
 crawl_runner = CrawlerRunner()      
 quotes_list = []                   
 scrape_in_progress = False
 scrape_complete = False
+
+#página inicial da aplicação com link para os endpoints
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
 
 @app.route('/crawl')
 def crawl_for_quotes():
@@ -20,41 +25,45 @@ def crawl_for_quotes():
     global scrape_in_progress
     global scrape_complete
 
+    #se não iniciou raspagem atualiza o valor do semáforo para True
+    #inicia a rapsgem e verifica get_result
     if not scrape_in_progress:
         scrape_in_progress = True
         global quotes_list
         scrape_with_crochet(quotes_list)
-        return get_results()
+    #se a rapagem estiver completa salva os dados em quotes_list
+    #retorna o template com a lista com notícias raspadas
     elif scrape_complete:
         save(quotes_list)
-        return jsonify(quotes_list)
-    return 'SCRAPE IN PROGRESS ATUALIZE A PÁGINA'
-
-def get_results():
-    
+        return render_template('crawl.html', quotes_list=quotes_list)
+    return render_template('crawl.html')
+"""
+def get_results():    
     global scrape_complete
     if scrape_complete:
-        return jsonify(quotes_list)
+        return render_template('crawl.html', quotes_list=quotes_list)
     return crawl_for_quotes()
-
+"""
 @crochet.run_in_reactor
 def scrape_with_crochet(_list):
     eventual = crawl_runner.crawl(NewsSpider, quotes_list=_list)
     eventual.addCallback(finished_scrape)
 
-def finished_scrape(null):
-    
+#sinaliza quando termina a raspagem
+def finished_scrape(null):    
     global scrape_complete
     scrape_complete = True
 
 def save(_list):
-    fl = open('notices.json', 'a')
+    buscar = 'Ibovespa'
+    fl = open('notices.json', 'w')
     elementos = []
     for lista in _list:
         for element in lista:
-            if re.search('\\bB3\\b', element, re.IGNORECASE):
+            if re.search('\\b'+buscar+'\\b', element, re.IGNORECASE):
                 elementos.append(element)
-    fl.write(str(elementos))                         
+    if len(elementos) > 0:
+        fl.write(str(elementos))                         
     fl.close()
       
 @app.route('/extract')
@@ -65,7 +74,5 @@ def extract_entities():
     entities = []
     for entity in texto.ents:
         entities.append(entity.text)
-    if len(entities) == 0:
-        return 'Nenhuma notícia sobre a B3 encontrada nos domínios http://financenews.com.br/feed/ e https://www.ultimoinstante.com.br/feed/'
-
-    return jsonify({'entities' : entities})
+        
+    return render_template('extract.html',entities=entities)
